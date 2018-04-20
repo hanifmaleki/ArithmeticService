797615ab161950ca8ccab405693e7b5f60fe9cc9
hanif <hanif_1362@yahoo.com>
2018-04-20 14:39:46 +0200

    First Version of web client

--- public/arithmetic.js
+++ public/arithmetic.js
@@ -1,12 +1,40 @@
 var app = angular.module('application', []);
 
 app.controller('controller', function($scope, $http) {
-    $scope.count = 0;
+    $scope.requests = [];
+    $scope.exprPattern = /^\s*\d+\s*[-\/\*\+]\s*\d+\s*$/;
     $scope.myFunction = function() {
-        $http.get("http://localhost:8080/add")
+        var ex = $scope.expression + '';
+        res = ex.split(/[ +*-\/]/);
+        $scope.value3 = parseInt(res[0]);
+        $scope.value4 = parseInt(res[1]);
+        var request = {request:ex, status:"sent", answer:"NaN"};
+        $http.get("http://localhost:8080/add",  {
+                                                   params: { operator: "test",
+                                                   operand1: parseInt(res[0]),
+                                                   operand2: parseInt(res[1])}
+                                               })
             .then(function(response) {
-                $scope.myVar = response.data;
+                //$scope.myVar = response.data;
+                request.status="processed at server"
             });
-        //$scope.myVar = "sent";
+       $scope.requests.push(request);
+       $scope.expression = "";
     }
 });
+
+/*app.directive('exprValid', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attr, controller) {
+      function myValidation(value) {
+        var re = /^\s*\d+\s*[-\/\*\+]\s*\d+\s*$/;
+        scope.checked=!re.test(value);
+        controller.$setValidity('expr', re.test(value));
+        return re.test(value);
+      }
+      controller.$parsers.push(myValidation);
+    }
+  };
+});*/
+
