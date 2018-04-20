31fdc344013e15b315fcd7208b355ef5f36dcedc
hanif <hanif_1362@yahoo.com>
2018-04-21 00:56:15 +0200

    Batch Request in client Config file

--- public/arithmetic.js
+++ public/arithmetic.js
@@ -12,28 +12,60 @@ app.controller('controller', function($scope, $http) {
     $scope.exprPattern = /^\s*\d+\s*[-\/\*\+]\s*\d+\s*$/;
     $scope.clientId = Math.floor((Math.random() * 1000) + 1);
     $scope.requestId = 1;
+    $scope.batchSize = 5 ;
     $scope.myFunction = function() {
         var ex = $scope.expression + '';
         res = ex.split(/[+*-\/]/);
         var op = ex.split(/\s*\d+s*/);
         //$scope.value3 = op[1];
         //$scope.value4 = op;
-        var request = {request:ex, status:"sent", answer:"NaN"};
-        $http.get(__env.apiUrl,  {
-                                                   params: { operator: op[1].trim(),
-                                                   operand1: parseInt(res[0]),
-                                                   operand2: parseInt(res[1]),
-                                                   clientId: $scope.clientId,
-                                                   id: $scope.requestId}
-                                               })
-            .then(function(response) {
-                request.status="processed at server"
-            });
-       $scope.requestId++;
-       $scope.requests.push(request);
+        sendRequest($scope, $http, ex, op[0].trim(), res[0], res[1]);
        $scope.expression = "";
     }
+
+    $scope.sendBatch = function(){
+        for(var i=0; i < $scope.batchSize; i++){
+            var op = "";
+            var rand = Math.floor((Math.random() * 4));
+            switch(rand){
+                case 0:
+                    op="+";
+                    break;
+                case 1:
+                    op = "-";
+                    break;
+                case 2:
+                    op = "*";
+                    break;
+                case 3:
+                    op = "/";
+                    break;
+            }
+            var n1 = Math.floor((Math.random() * 100));
+            var n2 = Math.floor((Math.random() * 100));
+            var ex = ""+ n1 + op + n2 ;
+            sendRequest($scope, $http, ex, op, n1, n2);
+        }
+    }
 });
+
+
+function sendRequest(scope, http, ex, operator, operand1, operand2){
+     var request = {request:ex, status:"sent", answer:"NaN"};
+             http.get(__env.apiUrl,  {
+                                                        params: { operator,
+                                                        operand1: operand1,
+                                                        operand2: operand2,
+                                                        clientId: scope.clientId,
+                                                        id: scope.requestId}
+                                                    })
+                 .then(function(response) {
+                     request.status="processed at server"
+                 });
+            scope.requestId++;
+            scope.requests.push(request);
+}
+
 
 /*app.directive('exprValid', function() {
   return {
